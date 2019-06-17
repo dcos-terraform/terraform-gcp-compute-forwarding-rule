a28f71b152c8b98e6138526d2cacbc0175cdd75a
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-17 23:34:10 +0200

    splat syntax, just take the first element, cleaner

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ip_address" {
   description = "Load balancer ip address"
-  value       = "${join(",", google_compute_address.forwarding_rule_address.*.address)}"
+  value       = "${element(google_compute_address.forwarding_rule_address.*.address, 0)}"
 }
